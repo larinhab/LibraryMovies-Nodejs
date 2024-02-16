6e420e90fde7c7afc5333cac6a51e5ffa6d1c70c
larinhab <larabviana@outlook.com>
2024-02-16 17:42:30 -0300

    :bug: fix code

--- src/database/Knex/migrations/20240118195119_createTags.js
+++ src/database/Knex/migrations/20240118195119_createTags.js
@@ -3,7 +3,7 @@ const knex = require("knex")
 
 exports.up = knex => knex.schema.createTable("tags", table => {
     table.increments("id")
-    table.text("tag_name").notNullable()
+    table.text("tag_name")
     
     table.text("note_id").references("id").inTable("notes").onDelete("CASCADE")
     table.integer("user_id").references("id").inTable("users")
