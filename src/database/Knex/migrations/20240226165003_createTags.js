7c196dfb06dcf38be6041b09951677e0e794e994
larinhab <larabviana@outlook.com>
2024-02-26 13:51:15 -0300

    :new: tabela de tags atualizada

--- src/database/Knex/migrations/20240226165003_createTags.js
+++ src/database/Knex/migrations/20240226165003_createTags.js
@@ -4,7 +4,7 @@ exports.up = knex => knex.schema.createTable("tags", table => {
     table.increments("id")
     table.text("tag_name")
     
-    table.text("note_id").references("id").inTable("notes").onDelete("CASCADE")
+    table.integer("note_id").references("id").inTable("notes").onDelete("CASCADE")
     table.integer("user_id").references("id").inTable("users")
 });
 
