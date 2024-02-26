54e7766c8dec357c372bfe9b7e4b2c06607e95a3
larinhab <larabviana@outlook.com>
2024-02-26 10:21:20 -0300

    :boom: criando a rota para mudar avatar

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,13 +1,21 @@
-const ensureAuthenticated = require('../middleware/ensureAuthenticated')
+const UserAvatarController = require("../controllers/userAvatarController")
+const ensureAuthenticated = require('../middleware/ensureAuthenticated') // PARA SABER QUEM É O USER
 const UserController = require("../controllers/userController")
+
+const uploadConfig = require("../configs/upload")
 const { Router } = require("express")
+const multer = require("multer")
 
 
 const usersRoutes = Router()
-const userController = new UserController
+const userController = new UserController()
+const userAvatarController = new UserAvatarController()
+
+const upload = multer(uploadConfig.MULTER)
 
 usersRoutes.post('/', userController.create)
 usersRoutes.put('/',ensureAuthenticated, userController.uptade)
 usersRoutes.delete('/', ensureAuthenticated, userController.delete)
+usersRoutes.patch('/avatar', ensureAuthenticated, upload.single("avatar"), userAvatarController.uptade) //PARA ATUALIZAR UM CAMPO ESPECIFICO (neste caso, avatar no user)
 
 module.exports = usersRoutes
