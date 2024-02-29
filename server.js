93c2920bd6763bd6c16d5341bae759eed6340e43
larinhab <larabviana@outlook.com>
2024-02-29 10:12:42 -0300

    :new: add api rest

--- server.js
+++ server.js
@@ -2,12 +2,14 @@ require("express-async-errors")
 
 const express = require("express") // IMPORTEI EXPRESS - 1
 const app = express() // INICIALIZEI EXPRESS - 2
+const cors = require("cors") // POSSIBILIDA QUE O BACK ATENDA AS REQUISIÇÕES DO FRONT
 
 const req = require("express/lib/request")
 app.use(express.json()) // DEFININDO O PADRÃO DAS REQUISIÇOES
 
 const routes = require("./src/routes")
 app.use(routes)
+app.use(cors()) // USANDO O CORS
 
 // CONECTANDO SQL LITE
 
@@ -20,6 +22,9 @@ app.listen(port, () => console.log(`Server is running on port: ${port}`)) // 4
 
 //
 const AppError = require("./src/utilis/AppError")
+
+const uploadConfig = require("./src/configs/upload")
+app.use("/files", express.static(uploadConfig.UPLOADS_FOLDER)) //
 
 app.use((error, request, response, next) => {
     if(error instanceof AppError) {
