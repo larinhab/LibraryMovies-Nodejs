8b143f07d95c985a211cce6b75a509d8d5740015
larinhab <larabviana@outlook.com>
2024-03-14 22:36:22 -0300

    :bug: fixing logic bugs

--- src/controllers/notesController.js
+++ src/controllers/notesController.js
@@ -30,6 +30,16 @@ class NotesController {
     return response.json();
   }
 
+  async delete(request, response){
+    const { id } = request.params
+
+    await knex("notes").where({ id }).delete()
+
+    return response.json({
+      message: "Filme excluído com sucesso"
+    })
+  }
+
   async show(request, response) {
     const { id } = request.params
 
@@ -46,50 +56,42 @@ class NotesController {
     })
   }
 
-  async delete(request, response){
-    const { id } = request.params
-
-    await knex("notes").where({ id }).delete()
-
-    return response.json({
-      message: "Filme excluído com sucesso"
-    })
-  }
-
   async index(request, response) { // CRIADA PARA LISTAR AS NOTAS
-    const { movie_title, tag_name } = request.query //PEGANDO O USUARIO DE UMA QUERY DENTRO DO INSOMINIA
+    const { movie_title, tags } = request.query //PEGANDO O USUARIO DE UMA QUERY DENTRO DO INSOMINIA
     const user_id = request.user.id
 
     let notes;
 
-    if(tag_name) {
-        const filterTags = tags.split(',').map(tag => tag.trim())
+    if(tags) {
+        const filterTags = tags.split(",").map((tag) => tag.trim());
 
         console.log(filterTags)
         
         notes = await knex("tags")
-            .whereIn("tag_name", filterTags)
-            .innerJoin("notes", "notes.id", "tags.note_id")
-            .groupBy("notes.id")
-            .orderBy("notes.movie_title")
-    } else {
+        .whereLike("tag_name", filterTags)
+        .innerJoin("notes", "notes.id", "tags.note_id")
+        .groupBy("notes.id")
+        .orderBy("notes.movie_title")
+      } else {
         notes = await knex("notes")
-            //.where({ user_id })
-            .whereLike("movie_title", `%${movie_title}%`)
-            .orderBy("movie_title");
-    }
-
-        const userTags = await knex("tags").where({ user_id });
-        const notesWithTags = notes.map(note => {
-              const noteTags = userTags.filter(tag => tag.note_id === note.id);
-
+        //.where({ user_id })
+        .whereLike("movie_title", `%${movie_title}%`)
+        .orderBy("movie_title");
+      }
+      
+      const userTags = await knex("tags")
+      const notesWithTags = notes.map(note => {
+        const noteTags = userTags.filter(tag => tag.note_id === note.id);
+        
+        
         return {
-            ...note,
-            tags: noteTags
+          ...note,
+          tags: noteTags
         }
     });
 
     return response.json(notesWithTags);
+    
 }
 }
 
